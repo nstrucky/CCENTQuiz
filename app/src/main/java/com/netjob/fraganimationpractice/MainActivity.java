0a5fd749367d7b7689b851c6087543171735f695
nstrucky <nickstruckmeyer@gmail.com>
2016-11-29 18:42:15 -0500

    Got the basic logic down for the CCENTQuizzer app

--- app/src/main/java/com/netjob/fraganimationpractice/MainActivity.java
+++ app/src/main/java/com/netjob/fraganimationpractice/MainActivity.java
@@ -7,47 +7,75 @@ import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView textView;
-    Button button;
+    private Button button;
+    protected static final String BUNDLE_STRING_KEY = "bundle_string_key";
+    protected static final String BUNDLE_INT_KEY = "bundle_int_key";
     
-    ArrayList<String> activeQuestionsArraylist;
-    String[] qustionsArray;    
+    ArrayList<String> activeQuestionsArrayList;
+    String[] questionsArray;
+    Random randomGenerator;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textView = (TextView) findViewById(R.id.text);
         button = (Button) findViewById(R.id.button);
+        randomGenerator = new Random();
 
         getSupportFragmentManager()
                 .beginTransaction()
                 .add(R.id.container, new OneFragment())
                 .commit();
                     
-        questionsArray = getResources().getStringArray(R.array.questions_array);
+        questionsArray = getResources()
+                        .getStringArray(R.array.questions_array);
         activeQuestionsArrayList = new ArrayList<String>(Arrays.asList(questionsArray));
 
     }
 
     protected void buttonListener(View view) {
 
-        TwoFragment aFragment = new TwoFragment();//add argument in constructor for question string?
-        Random randomGenerate = new Random();
-        int questionNumber = randomGenerate.nextInt(activeQuestionArrayList.size() - 1);
-        textView.setText(activeQuestionArrayList.get(questionNumber));
-        activeQuestionArrayList.remove(questionNumber);
+            if (activeQuestionsArrayList.size() > 1) {
 
-        getSupportFragmentManager()
-                .beginTransaction()
-                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
-                .replace(R.id.container, aFragment)
-                .addToBackStack(null)
-                .commit();
+                int questionNumber = randomGenerator.nextInt(activeQuestionsArrayList.size() - 1);
+
+                TwoFragment aFragment = new TwoFragment();
+                Bundle args = new Bundle();
+                args.putString(BUNDLE_STRING_KEY, "example String");
+                args.putInt(BUNDLE_INT_KEY, questionNumber);
+                aFragment.setArguments(args);
+
+                activeQuestionsArrayList.remove(questionNumber);
+
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                        .replace(R.id.container, aFragment)
+                        .addToBackStack(null)
+                        .commit();
+
+            } else {
+                Toast.makeText(getApplicationContext(), "Ya done", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+
+
+
+
+
+
+
 
 
     }
--- app/src/main/java/com/netjob/fraganimationpractice/TwoFragment.java
+++ app/src/main/java/com/netjob/fraganimationpractice/TwoFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 /**
@@ -14,16 +15,32 @@ import android.view.ViewGroup;
 public class TwoFragment extends Fragment {
 
 
+    TextView textView2;
+    String fromBundle;
+
     public TwoFragment() {
         // Required empty public constructor
     }
 
 
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_two, container, false);
+        View view = inflater.inflate(R.layout.fragment_two, container, false);
+
+        textView2 = (TextView) view.findViewById(R.id.text2);
+
+        fromBundle = getArguments().getString(MainActivity.BUNDLE_STRING_KEY, "nope");
+
+        textView2.setText(fromBundle);
+
+
+        return view;
     }
+
 
 }
